804aa6c1d17e6bb6ee885dd86a33e5b8e683fa2b
9ysn3 <yasseen.n.93@gmail.com>
2025-08-16 20:33:33 +0300

    main

--- Testing/obj/Debug/net9.0/Testing.AssemblyInfo.cs
+++ Testing/obj/Debug/net9.0/Testing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b814da90e9d8f471fd9272390775babf787a376a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f852578b42f74aa48541048c84d0105f27c7b419")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
